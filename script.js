40200a21b2d5ad4194f1acb3fa0f709f28ea8896
SakthiPoorani <78160466+SakthiPoorani@users.noreply.github.com>
2021-02-03 22:06:56 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyAs4vg3FT-Y6abmdst-zfZZDSnayy9AtbU",
+    authDomain: "aca-25-sports-website.firebaseapp.com",
+    projectId: "aca-25-sports-website",
+    storageBucket: "aca-25-sports-website.appspot.com",
+    messagingSenderId: "375239615304",
+    appId: "1:375239615304:web:140a98bad439fbad2d097d"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
